2cc75fadbd1f880ca6d8f8d66ce62d4eebca974e
tim-hub <tim_w@outlook.com>
2018-11-13 19:39:42 +1300

    init with threading enum and inheritance examples

---
+++ examTesting/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+
+namespace examTesting
+{
+    class Program
+    {
+
+        // 7421 A.1
+        enum color:int
+        {
+            red=-3,
+            green,
+            blue
+        }
+
+        static void Main(string[] args)
+        {
+            //color.red = 1;
+            Console.WriteLine(""+(int)color.red);
+            Console.WriteLine("" + (int)color.green);
+            Console.WriteLine("" + (int)color.blue);
+
+
+            // inheritance example a.5
+            MyP theP = new MyP(1, 12);
+            Console.WriteLine(theP.calculateValue());
+
+            // multi threading a.11
+            BeginThread();
+
+
+        }
+
+        // a.11
+        public static void BeginThread(){
+            Thread thread = new Thread(BMethod);
+
+            thread.IsBackground = true; 
+
+            thread.Start();
+        }
+
+        public static void BMethod(){
+            // no output in console if it is backgorund
+            Console.WriteLine("bmethod");
+        }
+
+        //a.5
+        interface IProduct
+        {
+            int Quantity { get; }
+            double Cost { get; }
+            double calculateValue();
+        }
+
+        public class MyP:IProduct{
+
+            public int Quantity { get; }
+            public double Cost { get; }
+            public MyP(int q, double c){
+                this.Quantity = q;
+                Cost = c;
+            }
+            public double calculateValue(){
+                return Quantity * Cost;
+            }
+        }
+    }
+
+
+}
